665dda7b0b47a4725c6d28a7b0d003e4d328dfa2
aiens1 <89996232+aiens1@users.noreply.github.com>
2023-03-23 16:26:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,38 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("longnavbarmenu").style.top = "0";
-  } else {
-    document.getElementById("longnavbarmenu").style.top = "-50px";
-  }
-}
-
-const menu =document.querySelector('#mymenu');
-const menubar = document.querySelector('.buttonmenu')
-const menulist =() =>{
-    menu.classList.toggle('change');
-    menubar.classList.toggle('active');
-}
-menu.addEventListener('click',menulist)
-
+//ProjectPage
 function cards1(){
-    window.location.href= "#Home";
+    window.location.href= "https://github.com/aiens1/aiens1.github.io";
+}
+function cards2(){
+    window.location.href= "https://youtube.com/playlist?list=PL35BmA83pHkOzlkEDt0URfQJNEgySCRZF&si=EnSIkaIECMiOmarE"
 }
 function cards4(){
-    window.location.href = "#Contact-me";
+    window.location.href = "#contactMePage";
 }
 
+//formsectionJS
 function sendMail() {
-  var params = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value,
-  };
-
-  const serviceID = "service_r3leezp";
-  const templateID = "template_46odnip";
-
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
-
-    })
-    .catch(err=>console.log(err));
-
-}
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_r3leezp";
+    const templateID = "template_46odnip";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  }
+  //footer
+  document.getElementById("linkedin-icon").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/irfan-nur-priya-nugraha-0269bb1b7/", "_blank");
+  });
+  
